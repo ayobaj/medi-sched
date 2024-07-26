7357056df192e0a8f5937ba5136c4cd149776093
ayo baj <ayobaj007@gmail.com>
2024-07-26 15:22:45 +0100

    continuous progress on the home page and imported files that will be needed to build the app

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,44 @@
-import { Button } from "@/components/ui/button"
+import PatientForm from "@/components/forms/PatientForm"
+import Image from "next/image"
+import Link from "next/link"
+
+
 
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-white underline">Home</h1>
-      <Button>Touch me</Button>
+    <div className="flex h-screen max-h-screen">
+
+      {/* otp verification*/}
+
+      <section className="remove-scrollbar container my-auto">
+        
+        <div className="sub-container max-w-[496px]">
+
+          <div className="flex text-xl">
+            <span className="text-indigo-600">Medi</span>
+            <span className="text-white">-Sched</span>
+          </div>
+
+          <PatientForm/>
+
+          <div className="text-14-regular mt-20 justify-between">
+            <p className="justify-items-end text-dark-600 lg:text-left">© {new Date().getFullYear()} Medi-Sched</p>
+            <Link href='/?admin=true' className="text-indigo-600">
+              Admin
+            </Link>
+          </div>
+
+        </div>
+
+      </section>
+
+      <Image src='/assets/images/frm.jpeg'
+        alt='patient'
+        className="side-img max-w-[50%] object-cover"
+        height={1000}
+        width={1000}/>
+
     </div>
   )
 }
---
+++ components/forms/PatientForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PatientForm = () => {
+    return (
+        <div>
+            PAtientForm
+        </div>
+    )
+}
+
+export default PatientForm
