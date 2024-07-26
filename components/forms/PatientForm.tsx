82c2c6b49f08e0d6ab6a04551cb18a372515fb5a
ayo baj <ayobaj007@gmail.com>
2024-07-26 15:29:11 +0100

    imported the shadcn form and made use of the form schema from shadcn which indicates that it is a client file

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,4 +1,13 @@
-import React from 'react'
+"use client"
+
+//browser key word interactions and some event have to be fired.
+
+import { z } from "zod"
+
+const formSchema = z.object({
+    username: z.string().min(2).max(50),
+})
+ 
 
 const PatientForm = () => {
     return (
