c3b859f0b10656605ae9a18b5a5d0285fc48edfd
ayo baj <ayobaj007@gmail.com>
2024-07-30 13:53:11 +0100

    imported the checkbox from shadcn-ui then used it for the use case for the formfieldtype.checkbox which was used in building the consent and privacy field and rendered the label

--- components/GlobalForm.tsx
+++ components/GlobalForm.tsx
@@ -20,6 +20,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Select, SelectContent, SelectTrigger, SelectValue } from "./ui/select"
 import { Textarea } from "./ui/textarea"
+import { Checkbox } from "./ui/checkbox"
 
 
 
@@ -134,6 +135,23 @@ const {iconSrc, iconAlt, fieldType, placeholder, showTimeSelect, dateFormat, ren
                     />
                 </FormControl>
             )
+        
+        case FormFieldType.CHECKBOX: 
+        return(
+            <FormControl>
+                <div className="flex items-center gap-4">
+                    <Checkbox
+                        id={props.name}
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                    />
+
+                    <label htmlFor={props.name} className="checkbox-label">
+                        {props.label}
+                    </label>
+                </div>
+            </FormControl>
+        )
 
         default: break;
     }
