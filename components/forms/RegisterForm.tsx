995f85723fd79196e695996a9e17687db48a3ef6
ayo baj <ayobaj007@gmail.com>
2024-07-30 14:28:00 +0100

    Extraction the identifcation Document using a condition and converting the file to blob which is understandable by the browser

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -8,12 +8,12 @@ import { Form, FormControl  } from "@/components/ui/form"
 import GlobalForm from "../GlobalForm"
 import SubmitButton from "../SubmitButton"
 import { useState } from "react"
-import { UserFormValidation } from "@/lib/FormValidation"
+import { PatientFormValidation, UserFormValidation } from "@/lib/FormValidation"
 import { useRouter } from "next/navigation"
 import { createUser } from "@/lib/actions/patient.actions"
 import { FormFieldType } from "./PatientForm"
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
-import { Doctors, GenderOptions, IdentificationTypes } from "@/constants"
+import { Doctors, GenderOptions, IdentificationTypes, PatientFormDefaultValues } from "@/constants"
 import { Label } from "../ui/label"
 import { SelectItem } from "../ui/select"
 import Image from "next/image"
@@ -30,9 +30,10 @@ const RegisterForm = ({user}:{user:User}) => {
 
 
 // 1. Define your form.
-const form = useForm<z.infer<typeof UserFormValidation>>({
-    resolver: zodResolver(UserFormValidation),
+const form = useForm<z.infer<typeof PatientFormValidation>>({
+    resolver: zodResolver(PatientFormValidation),
     defaultValues: {
+    ...PatientFormDefaultValues,
     name: "",
     email: "",
     phone: "",
@@ -40,20 +41,25 @@ const form = useForm<z.infer<typeof UserFormValidation>>({
 })
 
 // 2. Define a submit handler.
-async function onSubmit({name, email, phone}: z.infer<typeof UserFormValidation>) {
-    
-    setIsLoading(true)
+async function onSubmit(values: z.infer<typeof PatientFormValidation>) {
 
+    setIsLoading(true);
+
+    let formData;
+
+    if(values.identificationDocument && values.identificationDocument.length > 0){
+        //Blob is a special version of file which a document can use
+        const blobFile = new Blob([values.identificationDocument[0]], {
+            type: values.identificationDocument[0].type,
+        })
+
+        formData = new FormData();
+        formData.append('blobFile', blobFile);
+        formData.append('fileName', values.identificationDocument[0].name)
+    }
     
     try {
-        const userData = { name, email, phone}; 
-        const user = await createUser(userData);
-
-        if (user && user.$id) {
-            router.push(`/patients/${user.$id}/register`);
-        } else {
-            console.error('User ID is missing or invalid');
-        }
+        
     } catch (error) {
         console.error('Error on form submit:', error);
     } finally {
