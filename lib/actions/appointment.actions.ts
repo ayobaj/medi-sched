b2eb5270db70724fb1a5ad0d3180c15d1c10e87e
ayo baj <ayobaj007@gmail.com>
2024-08-05 13:12:02 +0100

    got rid of a server action that is commented out that is not in use

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -101,53 +101,7 @@ export const updateAppointment = async ({appointmentId, userId, appointment, typ
 }
 
 
-//     try {
-//       const appointments = await databases.listDocuments(
-//         DATABASE_ID!,
-//         APPOINTMENT_COLLECTION_ID!,
-//         [Query.orderDesc("$createdAt")]
-//       );
-  
-  
-//       const initialCounts = {
-//         scheduledCount: 0,
-//         pendingCount: 0,
-//         cancelledCount: 0,
-//       };
-  
-//       const counts = (appointments.documents as Appointment[]).reduce(
-//         (acc, appointment) => {
-//           switch (appointment.status) {
-//             case "scheduled":
-//               acc.scheduledCount++;
-//               break;
-//             case "pending":
-//               acc.pendingCount++;
-//               break;
-//             case "cancelled":
-//               acc.cancelledCount++;
-//               break;
-//           }
-//           return acc;
-//         },
-//         initialCounts
-//       );
-  
-//       const data = {
-//         totalCount: appointments.total,
-//         ...counts,
-//         documents: appointments.documents,
-//       };
-  
-//       return parseStringify(data);
-//     } catch (error) {
-//       console.error(
-//         "An error occurred while retrieving the recent appointments:",
-//         error
-//       );
-//     }
-// };
 
 
-  
+
 
